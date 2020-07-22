35539745c994dad205b74213d122580c6849c035
Alanhawk <alalanruiz@gmail.com>
2020-07-22 03:08:13 +0200

    Updates

---
+++ precache-manifest.380e3b01a65fc83f397a4c23184d8d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8082e019b46859a794e134eed302aa8d",
+    "url": "/cita-vet/index.html"
+  },
+  {
+    "revision": "e3dfa9471f547fde3262",
+    "url": "/cita-vet/static/css/main.b8a6c26f.chunk.css"
+  },
+  {
+    "revision": "0012f5a03a153668762a",
+    "url": "/cita-vet/static/js/2.2aca169f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cita-vet/static/js/2.2aca169f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3dfa9471f547fde3262",
+    "url": "/cita-vet/static/js/main.d260ee05.chunk.js"
+  },
+  {
+    "revision": "7095ba92d3f818b68428",
+    "url": "/cita-vet/static/js/runtime-main.8c1150ab.js"
+  }
+]);
